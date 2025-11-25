ca8c05d40a847f7d8e2c3480b83c55ed6520c37d
Pradeep <pratzy10@gmail.com>
2025-11-25 18:01:45 +0530

    Datatype Session Day 2 and Initial Commit

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+// Printing the Datatype String in log
+var Name = "Pradeep"
+console.log("The Datatype of the Variable Name is: "+typeof Name)
+
+// Printing Datatype Boolean in log
+var Do_you_like_Automation = true
+console.log("The Datatype of the Variable Do_you_like_Automation is: "+typeof Do_you_like_Automation)
+
+// Printing Datatype Number in log
+var Your_Weight = 75
+console.log("The Datatype of the Variable Your_Weight is: "+typeof  Your_Weight)
+
+//Printing Datatype undefined in log
+let Empty
+console.log("The Datatype of the Variable Empty is: "+typeof Empty)
+
+//Printing Datatype Null in log
+let Zilch = null
+console.log("The Datatype of the Variable Zilch is: "+ typeof Zilch)
